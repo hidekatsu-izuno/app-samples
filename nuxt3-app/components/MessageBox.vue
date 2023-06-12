f5c301d05aeeb10a04ec5b4b764e4ef9c2ea09f7
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-06-12 13:21:38 +0900

    Improve code

--- nuxt3-app/components/MessageBox.vue
+++ nuxt3-app/components/MessageBox.vue
@@ -49,32 +49,25 @@ const close = (result?: "ok" | "yes" | "no" | "cancel") => {
 
 <template>
   <Teleport to="body">
-    <div ref="elRef" class="MessageBox fixed bg-gray-900 bg-opacity-50 inset-0 z-40" :class="{ 'hidden': !modelValue }">
-      <div tabindex="-1"
-        class="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full justify-center items-center flex">
-        <div class="relative w-full h-full max-w-md md:h-auto">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" @click="() => close()"
-              class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
-              <div class="icon-[mdi--close-circle] text-2xl"></div>
-            </button>
-            <div class="p-6 text-center">
-              <div class="icon-[mdi--alert-circle-outline] text-red-500 text-6xl"></div>
-              <h3 class="mb-5 text-lg font-normal text-gray-500">{{ message }}</h3>
-              <div class="flex justify-center gap-0.5">
-                <button type="button" v-if="type === 'ok' || type === 'ok_cancel'" @click="() => close('ok')"
-                  class="PopupOkButton text-white bg-red-600 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">OK</button>
-                <button type="button" v-if="type === 'yes_no' || type === 'yes_no_cancel'" @click="() => close('yes')"
-                  class="PopupYesButton text-white bg-red-600 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">Yes</button>
-                <button type="button" v-if="type === 'yes_no' || type === 'yes_no_cancel'" @click="() => close('no')"
-                  class="PopupNoButton text-gray-500 bg-white hover:bg-gray-100 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">No</button>
-                <button type="button" v-if="type === 'ok_cancel' || type === 'yes_no_cancel'" @click="() => close('cancel')"
-                  class="PopupCancelButton text-gray-500 bg-white hover:bg-gray-100 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">キャンセル</button>
-              </div>
-            </div>
-          </div>
+    <dialog ref="elRef" class="MessageBox inset-0 rounded-lg shadow-2xl" :open="modelValue">
+      <button type="button" @click="() => close()"
+        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+        <div class="icon-[mdi--close-circle] text-2xl"></div>
+      </button>
+      <div class="p-6 text-center">
+        <div class="icon-[mdi--alert-circle-outline] text-red-500 text-6xl"></div>
+        <h3 class="mb-5 text-lg font-normal text-gray-500">{{ message }}</h3>
+        <div class="flex justify-center gap-0.5">
+          <button type="button" autofocus v-if="type === 'ok' || type === 'ok_cancel'" @click="() => close('ok')"
+            class="PopupOkButton text-white bg-red-600 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">OK</button>
+          <button type="button" autofocus v-if="type === 'yes_no' || type === 'yes_no_cancel'" @click="() => close('yes')"
+            class="PopupYesButton text-white bg-red-600 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">Yes</button>
+          <button type="button" v-if="type === 'yes_no' || type === 'yes_no_cancel'" @click="() => close('no')"
+            class="PopupNoButton text-gray-500 bg-white hover:bg-gray-100 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">No</button>
+          <button type="button" v-if="type === 'ok_cancel' || type === 'yes_no_cancel'" @click="() => close('cancel')"
+            class="PopupCancelButton text-gray-500 bg-white hover:bg-gray-100 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">キャンセル</button>
         </div>
       </div>
-    </div>
+    </dialog>
   </Teleport>
 </template>
