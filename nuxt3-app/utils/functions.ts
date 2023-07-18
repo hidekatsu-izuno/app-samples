7a77d077b45cafe28e57144b28ff662833a4baf0
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-07-18 22:38:57 +0900

    Update code

--- nuxt3-app/utils/functions.ts
+++ nuxt3-app/utils/functions.ts
@@ -1,10 +1,10 @@
 export {
-  v4 as uuidV4
-} from "uuid"
-
-export {
   parseDate,
   formatNumber,
   parseNumber,
   formatDate,
 } from "jtc-utils"
+
+export function uuid() {
+  return crypto.randomUUID()
+}
