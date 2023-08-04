dc3bd3b3b5a1a63604f81470ddd18a233545fccc
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-08-05 03:14:42 +0900

    Improve code

--- nuxt3-app/components/UIIcon.vue
+++ nuxt3-app/components/UIIcon.vue
@@ -6,19 +6,67 @@ const props = withDefaults(defineProps<{
 </script>
 
 <template>
-  <div v-if="props.name === 'alert-circle-outline'" class="UIIcon icon-[mdi--alert-circle-outline]" />
-  <div v-else-if="props.name === 'arrow-left'" class="UIIcon icon-[mdi--arrow-left]" />
-  <div v-else-if="props.name === 'arrow-right'" class="UIIcon icon-[mdi--arrow-right]" />
-  <div v-else-if="props.name === 'calendar'" class="UIIcon icon-[mdi--calendar]" />
-  <div v-else-if="props.name === 'check'" class="UIIcon icon-[mdi--check]" />
-  <div v-else-if="props.name === 'check-bold'" class="UIIcon icon-[mdi--check-bold]" />
-  <div v-else-if="props.name === 'chevron-left'" class="UIIcon icon-[mdi--chevron-left]" />
-  <div v-else-if="props.name === 'chevron-right'" class="UIIcon icon-[mdi--chevron-right]" />
-  <div v-else-if="props.name === 'circle-medium'" class="UIIcon icon-[mdi--circle-medium]" />
-  <div v-else-if="props.name === 'close-circle'" class="UIIcon icon-[mdi--close-circle]" />
-  <div v-else-if="props.name === 'help'" class="UIIcon icon-[mdi--help]" />
-  <div v-else-if="props.name === 'menu'" class="UIIcon icon-[mdi--menu]" />
-  <div v-else-if="props.name === 'menu-open'" class="UIIcon icon-[mdi--menu-open]" />
-  <div v-else-if="props.name === 'page-first'" class="UIIcon icon-[mdi--page-first]" />
-  <div v-else-if="props.name === 'page-last'" class="UIIcon icon-[mdi--page-last]" />
+  <div class="UIIcon" :data-name="name" />
 </template>
+
+<style>
+.UIIcon[data-name="alert-circle-outline"] {
+  @apply icon-[mdi--alert-circle-outline];
+}
+
+.UIIcon[data-name="arrow-left"] {
+  @apply icon-[mdi--arrow-left];
+}
+
+.UIIcon[data-name="arrow-right"] {
+  @apply icon-[mdi--arrow-right];
+}
+
+.UIIcon[data-name="calendar"] {
+  @apply icon-[mdi--calendar];
+}
+
+.UIIcon[data-name="check"] {
+  @apply icon-[mdi--check];
+}
+
+.UIIcon[data-name="check-bold"] {
+  @apply icon-[mdi--check-bold];
+}
+
+.UIIcon[data-name="chevron-left"] {
+  @apply icon-[mdi--chevron-left];
+}
+
+.UIIcon[data-name="chevron-right"] {
+  @apply icon-[mdi--chevron-right];
+}
+
+.UIIcon[data-name="circle-medium"] {
+  @apply icon-[mdi--circle-medium];
+}
+
+.UIIcon[data-name="close-circle"] {
+  @apply icon-[mdi--close-circle];
+}
+
+.UIIcon[data-name="help"] {
+  @apply icon-[mdi--help];
+}
+
+.UIIcon[data-name="menu"] {
+  @apply icon-[mdi--menu];
+}
+
+.UIIcon[data-name="menu-open"] {
+  @apply icon-[mdi--menu-open];
+}
+
+.UIIcon[data-name="page-first"] {
+  @apply icon-[mdi--page-first];
+}
+
+.UIIcon[data-name="page-last"] {
+  @apply icon-[mdi--page-last];
+}
+</style>
