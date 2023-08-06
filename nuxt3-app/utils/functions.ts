d4bcb6af07e732d704c58c07f9fbd3ee0ec4baef
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-08-06 13:49:56 +0900

    Improve code

--- nuxt3-app/utils/functions.ts
+++ nuxt3-app/utils/functions.ts
@@ -11,3 +11,28 @@ export {
 } from "jtc-utils"
 
 export { default as range } from "lodash-es/range"
+
+export const fetchURL = $fetch.create({
+  async onRequestError({ error }) {
+    const err = error as any
+    if (isNuxtError(err)) {
+      err.fatal = true
+      throw err
+    } else {
+      throw createError({
+        statusCode: err.statusCode,
+        statusMessage: err.statusMessage,
+        fatal: true,
+        cause: err,
+      })
+    }
+  },
+
+  async onResponseError({ response }) {
+    throw createError({
+      statusCode: response.status,
+      statusMessage: response.statusText,
+      fatal: true,
+    })
+  },
+})
--- nuxt3-app/utils/schemas.ts
+++ nuxt3-app/utils/schemas.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
 
-export const EmailSchema = z.string().nonempty().email().max(256)
-export const UserPasswordSchema = z.string().nonempty().min(8)
-export const UserNameSchema = z.string().nonempty().max(1024)
+export const EmailSchema = z.string().email().max(256)
+export const UserPasswordSchema = z.string().min(8)
+export const UserNameSchema = z.string().max(1024)
